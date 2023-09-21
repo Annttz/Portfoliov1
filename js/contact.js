78652930d51ad98eab525022f3e6c774af59d443
Annttz <120655501+Annttz@users.noreply.github.com>
2023-09-21 15:37:27 +0200

    new portfolio

---
+++ js/contact.js
@@ -0,0 +1,39 @@
+function sendMail() {
+
+  var name = document.getElementById("name").value;
+  var message = document.getElementById("message").value;
+
+  const serviceID = "service_98qpjx6";
+  const templateID = "template_ljz00dn";
+  const regExEmail = (value) => {
+    return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value);
+  }
+
+  function emailControl() {
+    const leMail = document.getElementById("email").value;
+    if (regExEmail(leMail)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  $('#myForm').on('submit', function (event) {
+    event.preventDefault();
+  });
+
+  if (name != "" && emailControl() != false && message != "") {
+    const params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    };
+
+    emailjs.send(serviceID, templateID, params);
+    swal("🎉", "Votre message c'est envoyé!", "success");
+    
+  } else {
+      swal("🤔", "Votre message ne s'est pas envoyé! Verifiez les champs.", "error");
+  }
+
+}
