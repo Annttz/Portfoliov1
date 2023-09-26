05f6afa88758c7cf70f3779aee6e26ab1fa58832
Annttz <120655501+Annttz@users.noreply.github.com>
2023-09-26 14:36:21 +0200

    test

--- js/js.js
+++ js/js.js
@@ -37,139 +37,141 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 		footer();
 	let mm = gsap.matchMedia();
-	
-	function activerAnimation() {
-		//header
-		function header() {
-			gsap.to('.title_paralax', {
-				scrollTrigger: {
-					trigger: '.header',
-					start: 'top top',
-					scrub: 1.9
-				},
-				yPercent: -150
-			})
-			gsap.to('.header .stroke', {
-				scrollTrigger: {
-					trigger: '.header',
-					start: 'top top',
-					scrub: 1.9
-				},
-				xPercent: 50
-			})
+	mm.add("(min-width: 768px)", () => {
+		// function activerAnimation() {
+			//header
+			function header() {
+				gsap.to('.title_paralax', {
+					scrollTrigger: {
+						trigger: '.header',
+						start: 'top top',
+						scrub: 1.9
+					},
+					yPercent: -150
+				})
+				gsap.to('.header .stroke', {
+					scrollTrigger: {
+						trigger: '.header',
+						start: 'top top',
+						scrub: 1.9
+					},
+					xPercent: 50
+				})
 		
-			gsap.to('.header__marq-wrapp', {
-				scrollTrigger: {
-					trigger: '.header',
-					start: 'top top',
-					scrub: 1.9
-				},
-				xPercent: -50
-			})
-			gsap.to('.header__marq-star img', {
-				scrollTrigger: {
-					trigger: '.header',
-					start: 'top top',
-					scrub: 1.9
-				},
-				rotate: -720
-			})
-		}
-		header();
+				gsap.to('.header__marq-wrapp', {
+					scrollTrigger: {
+						trigger: '.header',
+						start: 'top top',
+						scrub: 1.9
+					},
+					xPercent: -50
+				})
+				gsap.to('.header__marq-star img', {
+					scrollTrigger: {
+						trigger: '.header',
+						start: 'top top',
+						scrub: 1.9
+					},
+					rotate: -720
+				})
+			}
+			header();
 
 
-		//about
-		function about() {
-			gsap.from('.about__img', {
+			//about
+			function about() {
+				gsap.from('.about__img', {
+					scrollTrigger: {
+						trigger: '.about',
+						start: 'top bottom',
+						scrub: 1.9
+					},
+					yPercent: 80
+				})
+
+				gsap.from('.about__img img', {
+					scrollTrigger: {
+						trigger: '.about',
+						start: 'top bottom',
+						scrub: 1.9
+					},
+					scale: 1.6
+				})
+			}
+			gsap.to('.about__txt', {
 				scrollTrigger: {
-					trigger: '.about',
+					trigger: '.about__wrapp',
 					start: 'top bottom',
 					scrub: 1.9
 				},
-				yPercent: 80
+				yPercent: 50
 			})
-
-			gsap.from('.about__img img', {
-				scrollTrigger: {
-					trigger: '.about',
-					start: 'top bottom',
-					scrub: 1.9
-				},
-				scale: 1.6
-			})
-		}
-		gsap.to('.about__txt', {
-			scrollTrigger: {
-				trigger: '.about__wrapp',
-				start: 'top bottom',
-				scrub: 1.9
-			},
-			yPercent: 50
-		})
 	
-		about();
+			about();
 
 
-		//benefits
-		function benefits() {
-			gsap.from('.benefits__num', {
-				x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
-				scrollTrigger: {
-					trigger: '.benefits__list',
-					start: 'top bottom',
-					scrub: 1.9
-				}
-			})
-		}
-		benefits();
+			//benefits
+			function benefits() {
+				gsap.from('.benefits__num', {
+					x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+					scrollTrigger: {
+						trigger: '.benefits__list',
+						start: 'top bottom',
+						scrub: 1.9
+					}
+				})
+			}
+			benefits();
 
 
-		//portfolio
-		function portfolio() {
+			//portfolio
+			function portfolio() {
 
-			gsap.from('.work__item-img img', {
-				scale: 1.6,
-				scrollTrigger: {
-					trigger: '.work__wrapp',
-					start: 'top bottom',
-					scrub: 1.9
-				}
-			})
-		}
-		portfolio();
+				gsap.from('.work__item-img img', {
+					scale: 1.6,
+					scrollTrigger: {
+						trigger: '.work__wrapp',
+						start: 'top bottom',
+						scrub: 1.9
+					}
+				})
+			}
+			portfolio();
 
-		//serv
-		function serv() {
-			gsap.from('.serv__item-arrow', {
-				x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
-				scrollTrigger: {
-					trigger: '.serv__list',
-					start: 'top bottom',
-					scrub: 1.9
-				}
-			})
-		}
-		serv();
+			//serv
+			function serv() {
+				gsap.from('.serv__item-arrow', {
+					x: (i, el) => (1 - parseFloat(el.getAttribute('data-speed'))),
+					scrollTrigger: {
+						trigger: '.serv__list',
+						start: 'top bottom',
+						scrub: 1.9
+					}
+				})
+			}
+			serv();
 	
 
 
-		}
-	if (window.innerWidth >= 768) {
-    activerAnimation();
-	}
-	window.addEventListener('resize', function() {
-    // Obtenir la largeur actuelle de la fenêtre
-    var largeurFenetre = window.innerWidth;
+		// }
+	});
+		
+// 	if (window.innerWidth >= 768) {
+//     activerAnimation();
+// 	}
+// 	window.addEventListener('resize', function() {
+//     // Obtenir la largeur actuelle de la fenêtre
+//     var largeurFenetre = window.innerWidth;
 
-    // Vérifier si la largeur est inférieure à 768 pixels
-    if (largeurFenetre < 768) {
-        // Désactiver ou détruire l'animation ScrollTrigger
-        ScrollTrigger.getAll().forEach(trigger => {
-            trigger.kill();
-        });
-    } else {
-        // Activer l'animation si la largeur est de 768 pixels ou plus
-        activerAnimation();
-    }
-});
+//     // Vérifier si la largeur est inférieure à 768 pixels
+//     if (largeurFenetre < 768) {
+//         // Désactiver ou détruire l'animation ScrollTrigger
+//         ScrollTrigger.getAll().forEach(trigger => {
+//             trigger.kill();
+//         });
+//     } else {
+//         // Activer l'animation si la largeur est de 768 pixels ou plus
+//         activerAnimation();
+//     }
+// });
 });
